a82226cc400cfc80eec8b4ab714450b83dd64c24
Artem <75182193+ShockSplash@users.noreply.github.com>
2021-04-10 20:05:58 +0400

    Add files via upload

---
+++ FirstAplicattion/FirstAplicattion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Metadata;
+using StackExchange.Redis;
+
+namespace FirstAplicattion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Application1 int the chat!");
+            var configuration = new ConfigurationOptions();
+            configuration.EndPoints.Add("localhost", 6379);
+            
+            var redis = ConnectionMultiplexer.Connect(configuration);
+            var db = redis.GetDatabase();
+            
+            Console.WriteLine("Вводите сообщения. Для выхода введите /e");
+            string str = "";
+            
+            var channel = redis.GetSubscriber().Subscribe("ChatChannel");
+            channel.OnMessage(message =>
+            {
+                Console.WriteLine($"Application2:{message}");
+            });
+            
+            while (str != "/e")
+            {
+                //SubScribe
+                str = Console.ReadLine();
+                //Publish
+                db.Publish("ChatChannel2", str);
+                
+            }
+
+        }
+    }
+}
