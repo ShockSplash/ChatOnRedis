cf864b0c058e42dcec7280a56f8845595a7b6e53
Artem <zenit222555@mail.ru>
2021-04-10 19:01:33 +0400

    ready

---
+++ SecondApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using StackExchange.Redis;
+
+namespace SecondApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Application2 int the chat!");
+            var configuration = new ConfigurationOptions();
+            configuration.EndPoints.Add("localhost", 6379);
+            
+            var redis = ConnectionMultiplexer.Connect(configuration);
+            var db = redis.GetDatabase();
+            
+            Console.WriteLine("Вводите сообщения. Для выхода введите /e");
+            string str = "";
+            
+            //SubScribe
+            var channel = redis.GetSubscriber().Subscribe("ChatChannel2");
+            channel.OnMessage(message =>
+            {
+                Console.WriteLine($"Application1:{message}");
+            });
+            
+            while (str != "/e")
+            {
+                str = Console.ReadLine();
+                //Publish
+                db.Publish("ChatChannel", str);
+                
+            }
+        }
+        
+    }
+}
